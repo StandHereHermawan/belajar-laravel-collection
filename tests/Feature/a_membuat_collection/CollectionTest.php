9de62337362a92f90533a6b83c97117924287c05
Arief-Karditya <nolifegansaya@gmail.com>
2024-08-26 10:02:12 +0700

    Reduce

--- tests/Feature/a_membuat_collection/CollectionTest.php
+++ tests/Feature/a_membuat_collection/CollectionTest.php
@@ -618,4 +618,25 @@ class CollectionTest extends TestCase
         self::assertNotNull($result);
         self::assertEquals(9, $result);
     }
+
+    public function testReduce()
+    {
+        $collection = collect([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+
+        $result = $collection->reduce(function ($carry, $item) {
+            return $carry + $item;
+        });
+
+        // reduce (1,2) = 3
+        // reduce (3,3) = 6
+        // reduce (6,4) = 10
+        // reduce (10,5) = 15
+        // reduce (15,6) = 21
+        // reduce (21,7) = 28
+        // reduce (28,8) = 36
+        // reduce (36,9) = 45
+
+        self::assertNotNull($result);
+        self::assertEquals(45, $result);
+    }
 }
